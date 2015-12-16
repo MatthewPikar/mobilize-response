6fdb4426b5c6f9f4378da4d7f50feca987170a29
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2015-12-15 20:40:21 -0500

    refactored the constructor to follow constructor(options) standard

--- response.js
+++ response.js
@@ -1,6 +1,14 @@
 "use strict";
 
 var _ = require('lodash');
+var fs = require('fs');
+var bunyan = require('bunyan'),
+    masterLog = bunyan.createLogger({
+        name: 'mobilize',
+        level: 'fatal',
+        serializers: {response: responseSerializer}
+    }),
+    infoLog, errorLog, debugLog;
 
 var status = {
     200:{
@@ -72,57 +80,187 @@ var status = {
     }
 };
 
+/*
+    Response(callback, args, context)
 
-// Response
-// --------
-// use:
-// var Response = require('response');
-// var response = new Response(args, callback)
-//      args -  Contains any arguments you would like for created reponses to share.
-//              Typically this will be something like {requestId: id}.  Any provided arguments
-//              will override corresponding http fields.
-//      callback - This is the function you would like the response to invoke.
-//              Typically this will be a passthrough from the parent functions callback.
-// Create response by calling make:
-// response.make(code, args);
-//      *code -  Correspond to the http status code which serves as the response wrapper.
-//      args -  Contains any arguments you would like for created response to have in addition
-//              to the http fields and arguments passed in the constructor.  Any provided
-//              arguments will override corresponding http fields and constructor arguments.
-//      returns - If callback was provided, it returns the response in the standard
-//              callback(error, response) format.  Otherwise it returns the response collection.
-// * - required argument
-module.exports = function response(args, callback) {
-    var self = this,
-        template = (typeof(args) !== "undefined") ? args : {},
-        clbk = (typeof(callback) !== "undefined") ? callback: false;
+    Creates and packages service responses into a standardized message format.  Serves
+    as a centralized control for logging based on message content
 
-    self.make = function(code, args){
-        if(!clbk){
-            if (typeof(code) !== 'number') return new Error("Code is missing or not a number");
-            if (typeof(status[code]) === 'undefined') return new Error("Provided code is invalid.");
+    use:
+    var Response = require('response');
+    var response = new Response(args, callback);
+
+    callback -  This is the function you would like the response to invoke upon finishing.
+                It follows the standard function(err, response) format.
+                Typically this will be a passthrough from the parent functions callback.
+    args -  Contains any arguments you would like for all reponses to share.
+            Typically this will be something like {requestId: id}.  Any provided arguments
+            will override corresponding code wrapper content.
+    context -   This is the context passed onto all  messages.  Context controls how messages
+                are handled.  As an example, the 'api' context will strip most error information
+                like the callstack and format status arguments into http format.
+*/
+module.exports = function response(options) {
+    var self = this;
+
+    self.options = _.extend({
+        context: '',
+        logPath: "logs/",
+        debug: false,
+        logInfo: false,
+        logClientErrors: false,
+        logInternalErrors: true,
+        responseTemplate: {}
+    }, options);
+
+    // make sure logPath exists and is writable
+    try {fs.accessSync(self.options.logPath, fs.W_OK)}
+    catch(err){fs.mkdirSync(self.options.logPath);}
+
+    // create logs
+    infoLog = masterLog.child({
+        widget_type: self.options.context,
+        streams: [
+            {
+                name: 'file', level: 'info', type: 'rotating-file',
+                path: self.options.logPath + 'info-' + self.options.context + ".json",
+                period: '2d', count: 1
+            }
+        ]});
+    errorLog = masterLog.child({
+        widget_type: self.options.context,
+        streams: [
+            {
+                name: 'file', level: 'error', type: 'rotating-file',
+                path: self.options.logPath + 'error-' + self.options.context + ".json",
+                period: '1w', count: 2
+            },
+            {name: 'stdout', level: 'error', stream: process.stdout}
+        ]});
+    debugLog = masterLog.child({
+        widget_type: self.options.context,
+        streams: [
+            {
+                name: 'debug', level: 'debug', type: 'rotating-file',
+                path: self.options.logPath + 'debug-' + self.options.context + ".json",
+                period: '4h', count: 0
+            }
+        ]});
+
+    /*
+     make(message, args)
+
+     Creates a new message and forwards it to the callback (if provided).
+
+     code - (required) Correspond to the http status code which serves as the response wrapper.
+     args - Arguments of the message.  Any arguments clashing with code wrapper content
+     will overwrite that part of the message.
+
+     returns - new message
+     */
+    self.make = function (code, args, clbck) {
+        if (typeof(code) !== 'number') {
+            var err1 = new Error("Code is missing or not a number");
+            clbk(err1, null);
+            return err1;
         }
-        else {
-            if (typeof(code) !== 'number') return clbk(new Error("Code is missing or not a number"), null);
-            if (typeof(status[code]) === 'undefined') return clbk(new Error("Provided code is invalid."), null);
+        if (typeof(status[code]) === 'undefined') {
+            var err2 = new Error("Provided code is invalid.");
+            clbk(err2, null);
+            return err2;
         }
+        clbck = (typeof(clbck) === "function") ? clbck : passthrough;
 
         var res = {};
-        res = _.merge(res, template);
+        res = _.merge(res, self.options.responseTemplate);
         res = _.merge(res, {status: status[code]});
         res = _.merge(res, args);
-//        console.log("\n\n" + JSON.stringify(res) + "\n\n");
-        if(!clbk) return res;
-        else return clbk(null, res);
+        res = _.merge(res, {context: self.options.context});
+
+        if ((res.status.code / 100 < 4)
+            && self.options.logInfo)
+            infoLog.info({response: res}, 'info');
+        else if ((res.status.code / 100 >= 4) && (res.status.code / 100 < 5)
+            && self.options.logClientErrors)
+            infoLog.info({response: res}, 'info');
+        else if ((res.status.code / 100 >= 5)
+            && self.options.logInternalErrors)
+            errorLog.error({response: res}, 'error');
+
+        if (self.options.debug) debugLog.debug({response: res}, 'debug');
+
+        if (clbck) clbck(null, res);
+        return res;
     };
 
-    self.forward = function(message, args){
+    /*
+     forward(message, args)
+
+     Forwards the provided message to the callback (if provided).
+
+     message - the message to forward
+     args -  arguments to add onto the message.  Any arguments clashing with message content
+     will overwrite that part of the message.
+
+     returns - new message
+     */
+    self.forward = function (message, args, clbck) {
+        clbck = (typeof(clbck) === "function") ? clbck : passthrough;
+
         var res = {};
-        res = _.merge(res, template);
+        res = _.merge(res, self.options.responseTemplate);
         res = _.merge(res, message);
         res = _.merge(res, args);
-//        console.log("\n\n" + JSON.stringify(res) + "\n\n");
-        if(!clbk) return res;
-        else return clbk(null, res);
+        res = _.merge(res, {context: self.options.context});
+
+        if ((res.status.code / 100 < 4)
+            && self.options.logInfo)
+                infoLog.info({response: res}, 'info');
+        else if ((res.status.code / 100 >= 4) && (res.status.code / 100 < 5)
+            && self.options.logClientErrors)
+                infoLog.info({response: res}, 'info');
+        else if ((res.status.code / 100 >= 5)
+            && self.options.logInternalErrors)
+                errorLog.error({response: res}, 'error');
+
+        if (self.options.debug) debugLog.debug({response: res}, 'debug');
+
+        res.http$ = {
+            status: res.status.code,
+            headers: {
+                'date': new Date().toUTCString(),
+                'content-type': 'application/json'
+            }
+        };
+        // don't forward details of internal errors
+        if (res.error && (res.status.code / 100 >= 5)) delete res.error;
+        // only forward simple status in http$ is forwarded
+        delete res.status;
+
+        if (clbck) clbck(null, res);
+        return res;
     };
 };
+
+function responseSerializer(message){
+    var res = [];
+
+    if(Array.isArray(message.resources) && message.resources.length > 0) {
+        for (var i = 0, len = message.resources.length; i < len; i++) {
+            res[i] = {
+                id: message.resources[i].id ? message.resources[i].id : 'empty',
+                name: message.resources[i].name ? message.resources[i].name : 'empty'
+            };
+        }
+    }
+
+    return {
+        req_id: message.requestId,
+        status: message.status.code,
+        latency: message.latency ? message.latency: '',
+        res: res,
+        req: (message.request ? message.request : ''),
+        err: (message.error ? message.error: '')
+    }}
+
+function passthrough(err, res){return null;}
